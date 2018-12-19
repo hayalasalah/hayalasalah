95c9163025023c7692f1f71ca18a58cb81b8ae79
Zuhair Parvez <zuhairparvez@gmail.com>
2018-12-18 19:41:30 -0800

    Update texas/nueces scraper

--- backend/src/scrapers/texas/nueces.ts
+++ backend/src/scrapers/texas/nueces.ts
@@ -4,45 +4,71 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import { DateTime } from "luxon";
+import fetch from "node-fetch";
 import { Browser } from "puppeteer";
-import { Prayer, PrayerTimeTable } from "../../types/PrayerTime";
-import {
-  abortMediaRequests,
-  arrayOfPrayersToDaySchedule,
-  getDataForXPath,
-  stringToDateTime
-} from "../utils";
+import { DaySchedule, Prayer, PrayerTimeTable } from "../../types/PrayerTime";
+import { stringToDateTime } from "../utils";
 
-function getXPathString(tableIndex: number) {
-  return `//*[@id="post-42"]/div/div[1]/div/div[1]/div[1]/table/tbody/tr[${tableIndex}]/td[2]`;
+// tslint:disable-next-line:no-var-requires
+const parseXml = require("@rgrove/parse-xml");
+
+function getText(iqamah: any, index: number): string {
+  return iqamah.children[index].children[0].text;
 }
 
-export async function scrape(browser: Browser): Promise<PrayerTimeTable> {
-  const page = await browser.newPage();
-  await abortMediaRequests(page);
+function getHourMinute(text: string): number[] {
+  const pieces = text.split(":");
+  return [parseInt(pieces[0], 10), parseInt(pieces[1], 10)];
+}
 
-  await page.goto("http://nuecesmosque.com", { waitUntil: "domcontentloaded" });
+function buildTime(base: DateTime, text: string): DateTime {
+  const [hour, minute] = getHourMinute(text);
+  return base.set({
+    hour,
+    minute
+  });
+}
 
-  const tableIndices = [4, 6, 7, 8, 9];
-  const times = await Promise.all(
-    tableIndices.map(async (tableIndex, index): Promise<Prayer> => {
-      const isFajr = index === 0;
-      const xpath = getXPathString(tableIndex);
-      let timeString = await getDataForXPath(page, xpath);
-      if (timeString.startsWith("**")) {
-        timeString = timeString.slice(2);
-      }
-      return {
-        confidence: 100,
-        iqamah: stringToDateTime(timeString, "h:mm", "America/Chicago", !isFajr)
-      };
-    })
-  );
+function parseDay(iqamah: any): DaySchedule {
+  const dateText = getText(iqamah, 1);
+  const baseDate = stringToDateTime(dateText, "y-MM-dd", "America/Chicago");
 
-  await page.close();
+  if (baseDate.year !== DateTime.local().year) {
+    return {};
+  }
 
-  const daySchedule = arrayOfPrayersToDaySchedule(times);
-  daySchedule.jummah = [
+  const fajr: Prayer = {
+    adhan: buildTime(baseDate, getText(iqamah, 5)),
+    iqamah: buildTime(baseDate, getText(iqamah, 7)),
+    confidence: 100
+  };
+
+  const zuhr: Prayer = {
+    adhan: buildTime(baseDate, getText(iqamah, 11)),
+    iqamah: buildTime(baseDate, getText(iqamah, 13)),
+    confidence: 100
+  };
+
+  const asr: Prayer = {
+    adhan: buildTime(baseDate, getText(iqamah, 17)),
+    iqamah: buildTime(baseDate, getText(iqamah, 19)),
+    confidence: 100
+  };
+
+  const isha: Prayer = {
+    adhan: buildTime(baseDate, getText(iqamah, 23)),
+    iqamah: buildTime(baseDate, getText(iqamah, 25)),
+    confidence: 100
+  };
+
+  const sunset = buildTime(baseDate, getText(iqamah, 21));
+  const maghrib: Prayer = {
+    adhan: sunset,
+    iqamah: sunset.plus({ minutes: 5 }),
+    confidence: 100
+  };
+  const jummah = [
     {
       confidence: 100,
       iqamah: stringToDateTime("13:05", "HH:mm", "America/Chicago")
@@ -53,5 +79,36 @@ export async function scrape(browser: Browser): Promise<PrayerTimeTable> {
     }
   ];
 
-  return [daySchedule];
+  return {
+    fajr,
+    zuhr,
+    asr,
+    maghrib,
+    isha,
+    jummah
+  };
+}
+
+export async function scrape(browser: Browser): Promise<PrayerTimeTable> {
+  const result = await fetch(
+    "http://iqamatime.com/members/nuecesmosque.imam-gmail/TimingsXML.xml"
+  );
+  const resultXMLstring = await result.text();
+  const xmlData: any = parseXml(resultXMLstring).children[0];
+
+  const timetable: PrayerTimeTable = [];
+  for (const child of xmlData.children) {
+    if (child.type === "text") {
+      continue;
+    }
+
+    const day = parseDay(child);
+    if (day.fajr === undefined) {
+      continue;
+    }
+
+    timetable.push(day);
+  }
+
+  return timetable;
 }
